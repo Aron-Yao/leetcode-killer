98f2adc9d915d128e2b2a25914055f541914e37f
fdingiit <fdingiit@gmail.com>
2016-09-13 15:47:45 +0800

    117. Populating Next Right Pointers in Each Node II

---
+++ algo/populating-next-right-pointers-in-each-node-ii/connect.c
@@ -0,0 +1,53 @@
+/**
+ * Definition for binary tree with next pointer.
+ * struct TrqeeLinkNode {
+ *  int val;
+ *  struct TreeLinkNode *left, *right, *next;
+ * };
+ *
+ */
+struct TreeNode* findNext(struct TreeLinkNode* root) {
+    if (root == NULL) {
+        return NULL;
+    }
+    if (root->left) {
+        return root->left;
+    }
+    if (root->right) {
+        return root->right;
+    }
+    
+    return findNext(root->next);
+}
+
+void connect(struct TreeLinkNode *root) {
+    struct TreeLinkNode* cur;
+    struct TreeLinkNode* pre;
+    struct TreeLinkNode* next;
+    struct TreeLinkNode* tmp;
+    
+    if (root == NULL) {
+        return;
+    } 
+    
+    pre = root;
+    cur = NULL;
+    
+    while (1) {
+        cur = pre;
+        pre = findNext(pre);
+        if (pre == NULL) {
+            return;
+        }
+        while (cur) {
+            if (cur->left && cur->right) {
+                cur->left->next = cur->right;
+            } 
+            if (cur->left || cur->right) {
+                tmp = (cur->left == NULL) ? (cur->right) : ((cur->right == NULL) ? (cur->left) : (cur->right));
+                tmp->next = findNext(cur->next);
+            }
+            cur = cur->next;
+        }
+    }
+}
